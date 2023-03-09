273490977bfbb4eca7839f18b9940abef3fc37b4
Toxic254 <cyrus_g@outlook.com>
2023-03-09 19:30:04 +0300

    Make precedences data-driven

--- sc/CodeAnalysis/Parser.cs
+++ sc/CodeAnalysis/Parser.cs
@@ -66,12 +66,6 @@ namespace Syron.CodeAnalysis
             return current;
         }
 
-        // Helper method to parse a number literal.
-        private ExpressionSyntax ParseExpression()
-        {
-            return ParseTerm();
-        }
-
         // MatchToken a token of the expected kind and consume it.
         private SyntaxToken MatchToken(SyntaxKind kind)
         {
@@ -88,41 +82,49 @@ namespace Syron.CodeAnalysis
         // Parse() 
         public SyntaxTree Parse()
         {
-            var expression = ParseTerm();
+            var expression = ParseExpression();
             var endOfFileToken = MatchToken(SyntaxKind.EndOfFileToken);
             return new SyntaxTree(expression, endOfFileToken, _diagnostics);
         }
 
-        // ParseTerm deals with addition and subtraction
-        private ExpressionSyntax ParseTerm()
+        // ParseExpression deals with precedence
+        // This means that it will parse the expression with the highest precedence first.
+        public ExpressionSyntax ParseExpression(int parentPrecedence = 0)
         {
-            var left = ParseFactor();
+            var left = ParsePrimaryExpression();
 
-            while (Current.Kind == SyntaxKind.PlusToken ||
-                   Current.Kind == SyntaxKind.MinusToken)
+            while (true)
             {
+                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+
+                if (precedence == 0 || precedence <= parentPrecedence)
+                    break;
+
                 var operatorToken = NextToken();
-                var right = ParseFactor();
+                var right = ParseExpression(precedence);
                 left = new BinaryExpressionSyntax(left, operatorToken, right);
             }
 
             return left;
         }
 
-        // ParseFactor deals with multiplication and division
-        private ExpressionSyntax ParseFactor()
+        // GetBinaryOperatorPrecedence returns the precedence of a binary operator token.
+        // This is used to determine whether an operator should be parsed before or after another.
+
+        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
         {
-            var left = ParsePrimaryExpression();
-
-            while (Current.Kind == SyntaxKind.StarToken ||
-                   Current.Kind == SyntaxKind.SlashToken)
+            switch (kind)
             {
-                var operatorToken = NextToken();
-                var right = ParsePrimaryExpression();
-                left = new BinaryExpressionSyntax(left, operatorToken, right);
-            }
+                case SyntaxKind.StarToken:
+                case SyntaxKind.SlashToken:
+                    return 2;
+                case SyntaxKind.PlusToken:
+                case SyntaxKind.MinusToken:
+                    return 1;
 
-            return left;
+                default:
+                    return 0;
+            }
         }
 
         // ParsePrimaryExpression deals with numbers and parentheses
