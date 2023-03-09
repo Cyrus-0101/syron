66a0e7d070eb32cc32c23968acdbd420159ee36b
Toxic254 <cyrus_g@outlook.com>
2023-03-09 16:39:33 +0300

    SyntaxKind enums is used to represent the different kinds of tokens that the lexer can produce

---
+++ sc/CodeAnalysis/SyntaxKind.cs
@@ -0,0 +1,28 @@
+//   _________
+//  /   _____/__.__._______  ____   ____  
+//  \_____  <   |  |\_  __ \/  _ \ /    \ 
+//  /        \___  | |  | \(  <_> )   |  \
+// /_______  / ____| |__|   \____/|___|  /
+//         \/\/                        \/ 
+
+namespace sc
+
+{
+    // The SyntaxKind enum is used to represent the different kinds of tokens that the lexer can produce.
+    enum SyntaxKind
+    {
+        NumberToken,
+        WhiteSpaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        EndOfFileToken,
+        BadToken,
+        NumberExpression,
+        BinaryExpression,
+        ParenthesizedExpression,
+    }
+}
