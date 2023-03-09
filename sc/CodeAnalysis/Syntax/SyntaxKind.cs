fb45ffee6c95d565dae956bcbd3f55984b6c5342
Toxic254 <cyrus_g@outlook.com>
2023-03-09 20:11:54 +0300

    Added unary expression to our syntaxkind

---
+++ sc/CodeAnalysis/Syntax/SyntaxKind.cs
@@ -0,0 +1,29 @@
+//   _________
+//  /   _____/__.__._______  ____   ____  
+//  \_____  <   |  |\_  __ \/  _ \ /    \ 
+//  /        \___  | |  | \(  <_> )   |  \
+// /_______  / ____| |__|   \____/|___|  /
+//         \/\/                        \/ 
+
+namespace Syron.CodeAnalysis.Syntax
+
+{
+    // The SyntaxKind enum is used to represent the different kinds of tokens that the lexer can produce.
+    public enum SyntaxKind
+    {
+        NumberToken,
+        WhiteSpaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        EndOfFileToken,
+        BadToken,
+        NumberExpression,
+        BinaryExpression,
+        UnaryExpression,
+        ParenthesizedExpression,
+    }
+}
