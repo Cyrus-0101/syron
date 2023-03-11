2d06aa51a5a939b4fc55525076761df8e1b4ccd1
Toxic254 <cyrus_g@outlook.com>
2023-03-11 16:08:30 +0300

    Created our Variable symbols to handle our own types

---
+++ Syron/CodeAnalysis/VariableSymbol.cs
@@ -0,0 +1,22 @@
+//   _________
+//  /   _____/__.__._______  ____   ____  
+//  \_____  <   |  |\_  __ \/  _ \ /    \ 
+//  /        \___  | |  | \(  <_> )   |  \
+// /_______  / ____| |__|   \____/|___|  /
+//         \/\/                        \/ 
+
+namespace Syron.CodeAnalysis
+{
+    public sealed class VariableSymbol
+    {
+        internal VariableSymbol(string name, Type type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public string Name { get; }
+
+        public Type Type { get; }
+    }
+}
