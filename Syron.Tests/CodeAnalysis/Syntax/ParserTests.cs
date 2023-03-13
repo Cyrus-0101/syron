5c4a6b1c1a57fe2f331ec190b3fa901408d58719
Toxic254 <cyrus_g@outlook.com>
2023-03-13 13:28:54 +0300

    Added Parser Tests entry

---
+++ Syron.Tests/CodeAnalysis/Syntax/ParserTests.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+
+using Syron.CodeAnalysis.Syntax;
+
+namespace Syron.Tests.CodeAnalysis.Syntax
+{
+    public class ParserTests
+    { }
+}
