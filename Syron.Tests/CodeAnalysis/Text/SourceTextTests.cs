3405af3f303dc9797889603bc3e1634c5baae847
Toxic254 <cyrus_g@outlook.com>
2023-03-20 00:55:43 +0300

    Created our SourceTextTests: Passing!

---
+++ Syron.Tests/CodeAnalysis/Text/SourceTextTests.cs
@@ -0,0 +1,27 @@
+using Xunit;
+
+using Syron.CodeAnalysis.Text;
+
+//   _________
+//  /   _____/__.__._______  ____   ____  
+//  \_____  <   |  |\_  __ \/  _ \ /    \ 
+//  /        \___  | |  | \(  <_> )   |  \
+// /_______  / ____| |__|   \____/|___|  /
+//         \/\/                        \/ 
+
+
+namespace Syron.Tests.CodeAnalysis.Syntax
+{
+    public class SourceTextTests
+    {
+        [Theory]
+        [InlineData(".", 1)]
+        [InlineData("\r\n", 2)]
+        [InlineData("\r\n\r\n", 3)]
+        public void SourceText_IncludesLastLine(string text, int expectedLineCount)
+        {
+            var sourceText = SourceText.From(text);
+            Assert.Equal(expectedLineCount, sourceText.Lines.Length);
+        }
+    }
+}
