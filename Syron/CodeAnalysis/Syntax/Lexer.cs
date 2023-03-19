0bbc93cac0349c05512a9db5d7c8593de3d344ab
Toxic254 <cyrus_g@outlook.com>
2023-03-19 23:35:03 +0300

    Added SourceText Reference

--- Syron/CodeAnalysis/Syntax/Lexer.cs
+++ Syron/CodeAnalysis/Syntax/Lexer.cs
@@ -6,7 +6,7 @@ namespace Syron.CodeAnalysis.Syntax
     internal sealed class Lexer
     {
         private readonly DiagnosticBag _diagnostics = new DiagnosticBag();
-        private readonly string _text;
+        private readonly SourceText _text;
 
         private int _position;
 
@@ -14,7 +14,7 @@ namespace Syron.CodeAnalysis.Syntax
         private SyntaxKind _kind;
         private object _value;
 
-        public Lexer(string text)
+        public Lexer(SourceText text)
         {
             _text = text;
         }
@@ -39,7 +39,7 @@ namespace Syron.CodeAnalysis.Syntax
         {
             _start = _position;
             _kind = SyntaxKind.BadToken;
-            _value = _text.Substring(_start, 0);
+            _value = null;
 
             switch (Current)
             {
@@ -153,7 +153,7 @@ namespace Syron.CodeAnalysis.Syntax
             var text = SyntaxFacts.GetText(_kind);
 
             if (text == null)
-                text = _text.Substring(_start, length);
+                text = _text.ToString(_start, length);
 
             return new SyntaxToken(_kind, _start, text, _value);
         }
@@ -171,9 +171,9 @@ namespace Syron.CodeAnalysis.Syntax
                 _position++;
 
             var length = _position - _start;
-            var text = _text.Substring(_start, length);
+            var text = _text.ToString(_start, length);
             if (!int.TryParse(text, out var value))
-                _diagnostics.ReportInvalidNumber(new TextSpan(_start, length), _text, typeof(int));
+                _diagnostics.ReportInvalidNumber(new TextSpan(_start, length), text, typeof(int));
 
             _value = value;
             _kind = SyntaxKind.NumberToken;
@@ -185,7 +185,7 @@ namespace Syron.CodeAnalysis.Syntax
                 _position++;
 
             var length = _position - _start;
-            var text = _text.Substring(_start, length);
+            var text = _text.ToString(_start, length);
             _kind = SyntaxFacts.GetKeywordKind(text);
         }
     }
