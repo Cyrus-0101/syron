c3b9a9cb0b59ffb8cf6ee2fd29feb4e6427d23a7
Toxic254 <cyrus_g@outlook.com>
2023-03-20 00:27:52 +0300

    Ensured last line is added to source text, empty or not

--- Syron/CodeAnalysis/Text/SourceText.cs
+++ Syron/CodeAnalysis/Text/SourceText.cs
@@ -76,7 +76,7 @@ namespace Syron.CodeAnalysis
                 }
             }
 
-            if (position > lineStart)
+            if (position >= lineStart)
                 AddLine(result, sourceText, position, lineStart, 0);
 
             return result.ToImmutable();
