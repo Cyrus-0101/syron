fd59673313f906d1598302d3c6c6e073d876f9b6
Toxic254 <cyrus_g@outlook.com>
2023-03-20 11:01:56 +0300

    Allow compilations to be chained The chaining allows subsequent submissions in the REPL to get access to previously declared variables.
    This also introduces scoping so that we can lookup variables and type
    check them properly.

--- Syron/CodeAnalysis/Binding/Binder.cs
+++ Syron/CodeAnalysis/Binding/Binder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Immutable;
 using System.Collections.Generic;
 using Syron.CodeAnalysis.Syntax;
 
@@ -6,30 +7,61 @@ namespace Syron.CodeAnalysis.Binding
 {
     internal sealed class Binder
     {
-        // This is a field that will be used to store any diagnostics that occur during the compilation.
         private readonly DiagnosticBag _diagnostics = new DiagnosticBag();
 
-        // This is a field that will be used to store the variables that are declared in the program.
-        private readonly Dictionary<VariableSymbol, object> _variables;
+        private BoundScope _scope;
 
-        // This is a constructor that will be used to initialize the _variables field.
-        public Binder(Dictionary<VariableSymbol, object> variables)
+        public Binder(BoundScope parent)
         {
-            _variables = variables;
+            _scope = new BoundScope(parent);
         }
 
-        // This is a property that will be used to return the diagnostics that have been collected during the compilation.
+        public static BoundGlobalScope BindGlobalScope(BoundGlobalScope previous, CompilationUnitSyntax syntax)
+        {
+            var parentScope = CreateParentScope(previous);
+            var binder = new Binder(parentScope);
+            var expression = binder.BindExpression(syntax.Expression);
+            var variables = binder._scope.GetDeclaredVariables();
+            var diagnostics = binder.Diagnostics.ToImmutableArray();
+
+            if (previous != null)
+                diagnostics = diagnostics.InsertRange(0, previous.Diagnostics);
+
+            return new BoundGlobalScope(previous, diagnostics, variables, expression);
+        }
+
+        private static BoundScope CreateParentScope(BoundGlobalScope previous)
+        {
+            var stack = new Stack<BoundGlobalScope>();
+            while (previous != null)
+            {
+                stack.Push(previous);
+                previous = previous.Previous;
+            }
+
+            BoundScope parent = null;
+
+            while (stack.Count > 0)
+            {
+                previous = stack.Pop();
+                var scope = new BoundScope(parent);
+                foreach (var v in previous.Variables)
+                    scope.TryDeclare(v);
+
+                parent = scope;
+            }
+
+            return parent;
+        }
+
         public DiagnosticBag Diagnostics => _diagnostics;
 
-
-        // Binds the expression and returns the expression with the correct type.
-        // Throws an exception if the type is not recognized.
         public BoundExpression BindExpression(ExpressionSyntax syntax)
         {
             switch (syntax.Kind)
             {
                 case SyntaxKind.ParenthesizedExpression:
-                    return BindParenthesizedExpression(((ParenthesizedExpressionSyntax)syntax).Expression);
+                    return BindParenthesizedExpression((ParenthesizedExpressionSyntax)syntax);
                 case SyntaxKind.LiteralExpression:
                     return BindLiteralExpression((LiteralExpressionSyntax)syntax);
                 case SyntaxKind.NameExpression:
@@ -40,38 +72,27 @@ namespace Syron.CodeAnalysis.Binding
                     return BindUnaryExpression((UnaryExpressionSyntax)syntax);
                 case SyntaxKind.BinaryExpression:
                     return BindBinaryExpression((BinaryExpressionSyntax)syntax);
-
                 default:
                     throw new Exception($"Unexpected syntax {syntax.Kind}");
             }
         }
 
-        // Binds the expression and returns the expression with the correct type.
-        private BoundExpression BindParenthesizedExpression(ExpressionSyntax expression)
+        private BoundExpression BindParenthesizedExpression(ParenthesizedExpressionSyntax syntax)
         {
-            return BindExpression(expression);
+            return BindExpression(syntax.Expression);
         }
 
-
-        // This function binds a literal expression to its bound representation.
-        // The bound representation is a BoundLiteralExpression object.
         private BoundExpression BindLiteralExpression(LiteralExpressionSyntax syntax)
         {
             var value = syntax.Value ?? 0;
             return new BoundLiteralExpression(value);
         }
 
-
-        // This code finds the variable that corresponds to the name in the NameExpressionSyntax node.
-        // If no such variable exists, it reports an error and returns a BoundLiteralExpression with a value of 0.
-        // Otherwise, it returns a BoundVariableExpression that represents the variable.
         private BoundExpression BindNameExpression(NameExpressionSyntax syntax)
         {
             var name = syntax.IdentifierToken.Text;
 
-            var variable = _variables.Keys.FirstOrDefault(v => v.Name == name);
-
-            if (variable == null)
+            if (!_scope.TryLookup(name, out var variable))
             {
                 _diagnostics.ReportUndefinedName(syntax.IdentifierToken.Span, name);
                 return new BoundLiteralExpression(0);
@@ -82,31 +103,24 @@ namespace Syron.CodeAnalysis.Binding
 
         private BoundExpression BindAssignmentExpression(AssignmentExpressionSyntax syntax)
         {
-            // This method is responsible for binding the assignment expression and returning a bound expression.
-            // The method will create a new variable symbol for the variable being assigned to.
-            // The method will also check if the variable already exists in the current scope.
-            // If it does, the method will remove it from the dictionary.
-            // The method will then return a new bound assignment expression for the variable and the assigned expression.
-
             var name = syntax.IdentifierToken.Text;
             var boundExpression = BindExpression(syntax.Expression);
 
-            var existingVariable = _variables.Keys.FirstOrDefault(v => v.Name == name);
+            if (!_scope.TryLookup(name, out var variable))
+            {
+                variable = new VariableSymbol(name, boundExpression.Type);
+                _scope.TryDeclare(variable);
+            }
 
-            if (existingVariable != null)
-                _variables.Remove(existingVariable);
-
-            var variable = new VariableSymbol(name, boundExpression.Type);
-
-            _variables[variable] = null;
+            if (boundExpression.Type != variable.Type)
+            {
+                _diagnostics.ReportCannotConvert(syntax.Expression.Span, boundExpression.Type, variable.Type);
+                return boundExpression;
+            }
 
             return new BoundAssignmentExpression(variable, boundExpression);
-
         }
 
-        // Binds a unary expression
-        // syntax: the syntax of the unary expression
-        // returns: a bound node representing the unary expression
         private BoundExpression BindUnaryExpression(UnaryExpressionSyntax syntax)
         {
             var boundOperand = BindExpression(syntax.Operand);
@@ -121,8 +135,6 @@ namespace Syron.CodeAnalysis.Binding
             return new BoundUnaryExpression(boundOperator, boundOperand);
         }
 
-        // Binds a binary expression by first binding the left and right expressions
-        // and then binding the binary operator.
         private BoundExpression BindBinaryExpression(BinaryExpressionSyntax syntax)
         {
             var boundLeft = BindExpression(syntax.Left);
