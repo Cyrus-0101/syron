12b4b21c966e54d2da9097c8b64d4cd49597f713
Toxic254 <cyrus_g@outlook.com>
2023-03-22 09:14:36 +0300

    Fixed our script location

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Add dotnet build script
 # To run it open Linux terminal and type `sh build.sh || bash build.sh`
 
-dotnet build
+dotnet build ./src/Syron.sln /nologo
 
 dotnet test ./src/Syron.Tests/Syron.Tests.csproj
