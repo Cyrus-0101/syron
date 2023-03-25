d74768a45579b5267505a13d07f38ca523b46127
Toxic254 <cyrus_g@outlook.com>
2023-03-25 18:22:01 +0300

    Added Abstract Symbol Type

---
+++ src/Syron/CodeAnalysis/Symbols/Symbol.cs
@@ -0,0 +1,21 @@
+//   _________
+//  /   _____/__.__._______  ____   ____  
+//  \_____  <   |  |\_  __ \/  _ \ /    \ 
+//  /        \___  | |  | \(  <_> )   |  \
+// /_______  / ____| |__|   \____/|___|  /
+//         \/\/                        \/ 
+
+namespace Syron.CodeAnalysis.Symbols
+{
+    public abstract class Symbol
+    {
+        private protected Symbol(string name)
+        {
+            Name = name;
+        }
+
+        public abstract SymbolKind Kind { get; }
+        public string Name { get; }
+    }
+
+}
--- src/Syron/CodeAnalysis/Symbols/VariableSymbol.cs
+++ src/Syron/CodeAnalysis/Symbols/VariableSymbol.cs
@@ -9,19 +9,17 @@ using System;
 
 namespace Syron.CodeAnalysis.Symbols
 {
-    public sealed class VariableSymbol
+    public sealed class VariableSymbol : Symbol
     {
-        internal VariableSymbol(string name, bool isReadOnly, Type type)
+        internal VariableSymbol(string name, bool isReadOnly, Type type) : base(name)
         {
-            Name = name;
             IsReadOnly = isReadOnly;
             Type = type;
         }
-
-        public string Name { get; }
         public bool IsReadOnly { get; }
         public Type Type { get; }
 
-        public override string ToString() => Name;
+        public override SymbolKind Kind => SymbolKind.Variable;
     }
+
 }
