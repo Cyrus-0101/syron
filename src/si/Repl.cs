c04abe9f6ce7d62bca40ce011da9ca6ddda544f4
Toxic254 <cyrus_g@outlook.com>
2023-07-05 11:03:37 +0300

    Null safety

--- src/si/Repl.cs
+++ src/si/Repl.cs
@@ -45,7 +45,7 @@ namespace Syron
             {
                 _lineRenderer = lineRenderer;
                 _submissionDocument = submissionDocument;
-                _submissionDocument.CollectionChanged += SubmissionDocumentChanged;
+                _submissionDocument.CollectionChanged += SubmissionDocumentChanged!;
                 _cursorTop = Console.CursorTop;
                 Render();
             }
@@ -215,6 +215,7 @@ namespace Syron
             document.Clear();
             document.Add(string.Empty);
             view.CurrentLine = 0;
+            view.CurrentCharacter = 0;
         }
 
         private void HandleEnter(ObservableCollection<string> document, SubmissionView view)
