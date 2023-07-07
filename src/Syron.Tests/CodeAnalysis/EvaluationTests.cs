cfd048e268d6505492f90fc26f2f55d7734e320a
Toxic254 <cyrus_g@outlook.com>
2023-07-07 13:42:51 +0300

    Added missing evaluator function for missing return file.

--- src/Syron.Tests/CodeAnalysis/EvaluationTests.cs
+++ src/Syron.Tests/CodeAnalysis/EvaluationTests.cs
@@ -186,6 +186,24 @@ namespace Syron.Tests.CodeAnalysis
         }
 
         [Fact]
+        public void Evaluator_FunctionReturn_Missing()
+        {
+            var text = @"
+                function [add](a: int, b: int): int
+                {
+                }
+            ";
+
+            var diagnostics = @"
+                ERROR: Not all code paths return a value.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+
+
+        [Fact]
         public void Evaluator_IfStatement_Reports_CannotConvert()
         {
             var text = @"
@@ -423,6 +441,171 @@ namespace Syron.Tests.CodeAnalysis
 
             var diagnostics = @"
                 ERROR: 'print' is not a function.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Void_Function_Should_Not_Return_Value()
+        {
+            var text = @"
+                function test()
+                {
+                    return [1]
+                }
+            ";
+
+            var diagnostics = @"
+                ERROR: Since the function 'test' does not return a value the 'return' keyword cannot be followed by an expression.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Function_With_ReturnValue_Should_Not_Return_Void()
+        {
+            var text = @"
+                function test(): int
+                {
+                    [return]
+                }
+            ";
+
+            var diagnostics = @"
+                ERROR: An expression of type 'int' is expected.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Not_All_Code_Paths_Return_Value()
+        {
+            var text = @"
+                function [test](n: int): bool
+                {
+                    if (n > 10)
+                       return true
+                }
+            ";
+
+            var diagnostics = @"
+                ERROR: Not all code paths return a value.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Expression_Must_Have_Value()
+        {
+            var text = @"
+                function test(n: int)
+                {
+                    return
+                }
+                const value = [test(100)]
+            ";
+
+            var diagnostics = @"
+                ERROR: Expression must have a value.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Theory]
+        [InlineData("[break]", "break")]
+        [InlineData("[continue]", "continue")]
+        public void Evaluator_Invalid_Break_Or_Continue(string text, string keyword)
+        {
+            var diagnostics = $@"
+                ERROR: The keyword '{keyword}' can only be used inside of loops.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Invalid_Return()
+        {
+            var text = @"
+                [return]
+            ";
+
+            var diagnostics = @"
+                ERROR: The keyword 'return' can only be used inside of functions.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Parameter_Already_Declared()
+        {
+            var text = @"
+                function sum(a: int, b: int, [a: int]): int
+                {
+                    return a + b + c
+                }
+            ";
+
+            var diagnostics = @"
+                ERROR: A parameter with the name 'a' already exists.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Function_Must_Have_Name()
+        {
+            var text = @"
+                function [(]a: int, b: int): int
+                {
+                    return a + b
+                }
+            ";
+
+            var diagnostics = @"
+                ERROR: Unexpected token <OpenParenthesisToken>, expected <IdentifierToken>.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Wrong_Argument_Type()
+        {
+            var text = @"
+                function test(n: int): bool
+                {
+                    return n > 10
+                }
+                const testValue = ""string""
+                test([testValue])
+            ";
+
+            var diagnostics = @"
+                ERROR: Function 'test' requires parameter 'n' to be of type 'int' but was given 'string'.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Bad_Type()
+        {
+            var text = @"
+                function test(n: [invalidtype])
+                {
+                }
+            ";
+
+            var diagnostics = @"
+                ERROR: Type 'invalidtype' doesn't exist.
             ";
 
             AssertDiagnostics(text, diagnostics);
