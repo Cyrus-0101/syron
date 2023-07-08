cf43db68b8e0e1e4c1e4f23b602bfcc797e4648a
Toxic254 <cyrus_g@outlook.com>
2023-07-08 22:58:19 +0300

    Added compiler project

---
+++ src/sc/Program.cs
@@ -0,0 +1,43 @@
+﻿using Syron.CodeAnalysis;
+using Syron.CodeAnalysis.Symbols;
+using Syron.CodeAnalysis.Syntax;
+using Syron.IO;
+
+namespace Syron
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("usage: sc <source-paths>");
+                return;
+            }
+
+            if (args.Length > 1)
+            {
+                Console.WriteLine("error: only one path supported right now");
+                return;
+            }
+
+            var path = args.Single();
+
+            var text = File.ReadAllText(path);
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            var compilation = new Compilation(syntaxTree);
+            var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+
+            if (!result.Diagnostics.Any())
+            {
+                if (result.Value != null)
+                    Console.WriteLine(result.Value);
+            }
+            else
+            {
+                Console.Error.WriteDiagnostics(result.Diagnostics, syntaxTree);
+            }
+        }
+    }
+}
