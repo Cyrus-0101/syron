c3fefb0bfa47a7dc6902c62e2e6ff021566636ec
Toxic254 <cyrus_g@outlook.com>
2023-08-03 17:04:26 +0300

    Refactor

--- src/Syron/CodeAnalysis/Symbols/TypeSymbol.cs
+++ src/Syron/CodeAnalysis/Symbols/TypeSymbol.cs
@@ -9,18 +9,18 @@ namespace Syron.CodeAnalysis.Symbols
 {
     public sealed class TypeSymbol : Symbol
     {
-        public static readonly TypeSymbol Bool = new TypeSymbol("bool");
-        public static readonly TypeSymbol Int = new TypeSymbol("int");
-        public static readonly TypeSymbol String = new TypeSymbol("string");
+
         public static readonly TypeSymbol Error = new TypeSymbol("?");
-        public static readonly TypeSymbol Void = new TypeSymbol("void");
         public static readonly TypeSymbol Any = new TypeSymbol("any");
+        public static readonly TypeSymbol Int = new TypeSymbol("int");
+        public static readonly TypeSymbol Bool = new TypeSymbol("bool");
+        public static readonly TypeSymbol String = new TypeSymbol("string");
+        public static readonly TypeSymbol Void = new TypeSymbol("void");
 
         private TypeSymbol(string name) : base(name)
         {
 
         }
-
 
         public override SymbolKind Kind => SymbolKind.Type;
     }
